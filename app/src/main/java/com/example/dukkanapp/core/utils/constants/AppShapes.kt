664760cb0406aa83877ee61a47cc939898499c95
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-09-15 21:23:06 +0100

    feat: add AppShapes for Material 3 theming support
    - Introduced `AppShapes.kt` to define Material 3 shape configurations for the app.
    - Configured `extraSmall`, `small`, `medium`, `large`, and `extraLarge` shapes using `RoundedCornerShape` mapped to `AppDimens` radius constants.

---
+++ app/src/main/java/com/example/dukkanapp/core/utils/constants/AppShapes.kt
@@ -0,0 +1,12 @@
+package com.example.dukkanapp.core.utils.constants
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+
+val AppShapes= Shapes(
+    extraSmall = RoundedCornerShape(AppDimens.RadiusXs),
+    small = RoundedCornerShape(AppDimens.RadiusSm),
+    medium = RoundedCornerShape(AppDimens.RadiusMd),
+    large = RoundedCornerShape(AppDimens.RadiusLg),
+    extraLarge = RoundedCornerShape(AppDimens.RadiusXl),
+)
