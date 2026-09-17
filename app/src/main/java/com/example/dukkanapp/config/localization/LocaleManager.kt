98a2d04c53d66a208d11331209db17054f7f8ed7
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-09-17 22:33:20 +0100

    feat: add LocaleManager for application localization
    - Introduce `LocaleManager` to manage app-wide locale settings using `AppCompatDelegate`.
    - Add `setAppLocal` method to dynamically set the application locale based on language tags.
    - Add `currentLocalTag` method to retrieve the active language tags.

---
+++ app/src/main/java/com/example/dukkanapp/config/localization/LocaleManager.kt
@@ -0,0 +1,16 @@
+package com.example.dukkanapp.config.localization
+
+import androidx.core.os.LocaleListCompat
+import androidx.appcompat.app.AppCompatDelegate
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class LocaleManager @Inject constructor() {
+    fun setAppLocal(languageCode:String){
+        val localList= LocaleListCompat.forLanguageTags(languageCode)
+        AppCompatDelegate.setApplicationLocales(localList)
+    }
+
+    fun currentLocalTag(): String? = AppCompatDelegate.getApplicationLocales().toLanguageTags().takeIf { it.isNotBlank() }
+}
