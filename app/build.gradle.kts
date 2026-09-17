ae8655516e772314223b099a023acb166802b356
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-09-17 21:51:01 +0100

    chore: add Hilt and KSP dependencies
    - Add Hilt and KSP version references, library declarations, and plugin definitions to `gradle/libs.versions.toml`
    - Declare `ksp` and `hilt.android` plugins in root `build.gradle.kts`
    - Apply KSP and Hilt plugins and add `hilt-android`, `hilt-navigation-compose`, and `hilt-compiler` dependencies in `app/build.gradle.kts`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -44,6 +46,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // hilt
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    ksp(libs.hilt.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
@@ -51,4 +58,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+
 }
